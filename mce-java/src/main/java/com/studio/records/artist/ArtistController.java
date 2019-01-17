93e09b360aec257b0ef71ee53f27717bedc6fb5f
Vilius Zalys <you@example.com>
2019-01-17 12:15:44 +0200

    thirdcommit

--- mce-java/src/main/java/com/studio/records/artist/ArtistController.java
+++ mce-java/src/main/java/com/studio/records/artist/ArtistController.java
@@ -1,4 +1,74 @@
 package com.studio.records.artist;
 
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@Api(value = "artist")
+@RequestMapping(value = "/api/artists")
 public class ArtistController {
+    private final ArtistService artistService;
+
+    @Autowired
+    public ArtistController(ArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value="Get artists", notes="Returns all artists")
+    public List<ArtistRestResp> getAllArtists() {
+        return artistService.getAllArtists();
+    }
+
+    @RequestMapping(path="/{pseaudonym}",method = RequestMethod.GET)
+    @ApiOperation(value="Get pseudonym", notes="Returns one artist pseudonym")
+    public ArtistRestResp getArtist(@ApiParam(value="Artist pseudonym", required=true)
+                                    @Valid
+                                    @PathVariable String pseudonym) {
+        return artistService.findStudioByStudioTitle()studioService.findStudioByStudioTitle(studioTitle);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value="Create studio", notes="Creates new studio")
+    public void createStudio(@ApiParam(value="studio data", required=true)
+                             @Valid
+                             @RequestBody final CreateStudioComm p) {
+        studioService.createStudio(p.getStudioTitle(), p.getLogo(), p.getCategory(), p.getSize());
+    }
+
+    @RequestMapping(path="/{studioTitle}",method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value="Edit studio",notes="Change studio data")
+    public void updateStudio(
+            @ApiParam(value="Studio title",required=true)
+            @Valid
+            @PathVariable final String title,
+            @ApiParam(value="Studio data",required=true)
+            @Valid
+            @RequestBody final CreateStudioComm p) {
+
+        studioService.updateStudio(title, p.getStudioTitle(), p.getLogo(), p.getCategory(), p.getSize());
+    }
+
+    @RequestMapping(path = "/{studioTitle}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value="Delete studio", notes="Deletes certain studio")
+    public void deleteStudio(@ApiParam(value="Studio title", required=true)
+                             @Valid
+                             @PathVariable final String studioTitle) {
+
+        studioService.deleteStudio(studioTitle);
+
+    }
+
 }
--- mce-java/src/main/java/com/studio/records/artist/ArtistService.java
+++ mce-java/src/main/java/com/studio/records/artist/ArtistService.java
@@ -1,4 +1,79 @@
 package com.studio.records.artist;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ArtistService {
+    @Autowired
+    private ArtistRepository artistRepository;
+
+
+    @Transactional(readOnly=true)
+    public List<ArtistRestResp> getAllArtists () {
+
+
+        return artistRepository.findAll().stream().map(
+                (artist) ->
+                        new ArtistRestResp(
+                                artist.getPseudonym(),
+                                artist.getFirstname(),
+                                artist.getSurname(),
+                                artist.getGenre(),
+                                artist.getCountry(),
+                                artist.getDateOfBirth(),
+                                artist.getPhoto()
+                .colle
+
+    }
+
+    @Transactional
+    public ArtistRestResp findArtistByPseudonym(String pseudonym) {
+        Artist artist = artistRepository.findArtistByPseudonym(pseudonym);
+        return new ArtistRestResp(artist.getPseudonym(),
+                artist.getFirstname(),
+                artist.getSurname(),
+                artist.getGenre(),
+                artist.getCountry(),
+                artist.getDateOfBirth(),
+                artist.getPhoto();
+    }
+
+    @Transactional
+    public void createStudio(String studioTitle,String logo, String category, String size) {
+        Studio studio = new Studio(studioTitle,logo, category, size);
+
+        studioRepository.save(studio);
+    }
+
+
+    @Transactional
+    public void deleteStudio(String studioTitle) {
+        studioRepository.deleteStudioByStudioTitle(studioTitle);
+    }
+
+    public StudioRepository getStudioRepository() {
+        return studioRepository;
+    }
+
+    public void setStudioRepository(StudioRepository studioRepository) {
+        this.studioRepository = studioRepository;
+    }
+
+
+    @Transactional
+    public void updateStudio(String newTitle, String studioTitle, String logo, String category, String size) {
+        Studio studioUpdate = studioRepository.findStudioByStudioTitle(newTitle);
+        studioUpdate.setTitle(studioTitle);
+        studioUpdate.setLogo(logo);
+        studioUpdate.setCategory(category);
+        studioUpdate.setSize(size);
+        studioRepository.save(studioUpdate);
+    }
+
 }
